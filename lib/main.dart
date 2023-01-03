9b1c77708935e912eaef04d599d73e6f4d58115e
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-01-03 11:18:24 +0900

    アジア追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// 入力された金額を保持する変数
+  double _amount = 0;
 
-  void _incrementCounter() {
+// コンボボックスで選択された通貨の種類を保持する変数
+  String? _currency = 'JPY';
+
+// 両替された円の金額を保持する変数
+  double _result = 0;
+
+// 為替レートを取得するAPIのURL
+  String _apiUrl = 'https://api.exchangerate-api.com/v4/latest/JPY';
+
+// 計算ボタンが押されたときの処理
+  void _calculate() async {
+// 入力された金額を取得する
+    double amount = _amount;
+
+// コンボボックスで選択された通貨の種類を取得する
+    String? currency = _currency;
+
+    var uri = Uri.parse(_apiUrl);
+
+// APIを使って、為替レートを取得する
+    var response = await http.get(uri);
+
+// APIから返されたJSONをパースする
+    Map<String, dynamic> data = json.decode(response.body);
+
+// 為替レートを取得する
+    double rate = data['rates'][currency];
+
+// 入力された金額を円に両替する
+    double result = amount * rate;
+
+// 両替された円の金額を保存する
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _result = result;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Currency Converter'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  _amount = double.parse(value);
+                });
+              },
+              decoration: InputDecoration(
+                labelText: '円を入力',
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(height: 80),
+            DropdownButton(
+              value: _currency,
+              items: [
+                DropdownMenuItem(
+                  value: 'AED',
+                  child: Text('AED - UAEディルハム'),
+                ),
+                DropdownMenuItem(
+                  value: 'THB',
+                  child: Text('THB - タイバーツ'),
+                ),
+                DropdownMenuItem(
+                  value: 'IDR',
+                  child: Text('IDR - インドネシアルピー'),
+                ),
+                DropdownMenuItem(
+                  value: 'MYR',
+                  child: Text('MYR - マレーシアリンギット'),
+                ),
+                DropdownMenuItem(
+                  value: 'PHP',
+                  child: Text('フィリピン・ペソ'),
+                ),
+                DropdownMenuItem(
+                  value: 'SGD',
+                  child: Text('シンガポール・ドル'),
+                ),
+                DropdownMenuItem(
+                  value: 'JPY',
+                  child: Text(
+                    'JPY - 日本円（選べません）',
+                    style: TextStyle(backgroundColor: Colors.grey),
+                  ),
+                  enabled: false,
+                ),
+                DropdownMenuItem(
+                  value: 'NZD',
+                  child: Text('NZD - ニュージーランドドル'),
+                ),
+                DropdownMenuItem(
+                  value: 'VND',
+                  child: Text('VND - ベトナムドン'),
+                ),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  _currency = value;
+                });
+              },
             ),
+            SizedBox(height: 80),
+            ElevatedButton(
+              child: Text('Calculate'),
+              onPressed: _calculate,
+            ),
+            SizedBox(height: 8),
+            Text('換算の結果: $_currencyで$_resultです'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
